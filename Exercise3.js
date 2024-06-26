5c6fb3c5c69dafca3450696d1dca313c000aa3d9
saneh <mkhizes834@gmail.com>
2024-06-26 19:33:16 +0200

    task3

---
+++ Exercise3.js
@@ -0,0 +1,23 @@
+ //Function that take two numbers//
+ 
+ function twoNumbers(num1,num2){
+return num1 + num2
+ };
+
+   
+ let result = twoNumbers(20,3);
+ console.log(result);
+
+ //RETURN FIXED NUMBER//
+ function returnfixedNumber(){
+  return 30;
+ }
+  
+ //UPPERCASE OR LOWECASE//
+ function upperOrlowe(str,bool = false){
+  if(bool){
+    return str.toUpperCase();
+  }else{
+    return str.toLowerCase();
+  }
+ }
---
+++ Variable-Declarations.js
@@ -0,0 +1,23 @@
+ //EXERCISE ONE: VARIABLE DECLATIONS//
+ let myName = "zee";
+
+ let Number = 10;
+ console.log(typeof Number);
+
+ //ANYTYPE//
+   let Anytype;
+   Anytype = "sanele" //STRING//
+   console.log(typeof Anytype);
+   
+   Anytype = 10; //NUMBER//
+   console.log(typeof Anytype)
+
+   Anytype = true //boleen//
+   console.log(typeof Anytype)
+
+   let stringNDnumber;
+   stringNDnumber = "me"; //string//
+   console.log(typeof stringNDnumber);
+
+   stringNDnumber = 10;//number//
+   console.log(typeof stringNDnumber);
